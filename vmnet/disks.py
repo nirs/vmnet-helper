d35abc6ae45f7858a1bc87968bc448f3c13b0ca0
Nir Soffer <nirsof@gmail.com>
2025-02-23 22:55:16 +0200

    Add optional image properties
    Add "kernel", "kernel_parameters", and "initrd" properties to image spec.
    If kernel and initrd are specified the urls are downloaded to the cache
    and the data is cloned to the vm directory. If kernel_parameters is
    specified it is copied to the disk dict.
    
    If the optional disk properties are specified we use them in the qemu
    command. This will be used to debug the random kernel panics in the
    github runner.
    
    We can use these properties later for vfkit and krunkit it needed.

--- vmnet/disks.py
+++ vmnet/disks.py
@@ -39,7 +39,21 @@ def create_disk(vm_name, distro):
     if not os.path.isfile(disk["image"]):
         image = create_image(image_info["image"], format="raw", size="20g")
         print(f"Creating image '{disk['image']}'")
-        clone(image, disk['image'])
+        clone(image, disk["image"])
+    if "kernel" in image_info:
+        disk["kernel"] = store.vm_path(vm_name, "kernel")
+        if not os.path.isfile(disk["kernel"]):
+            kernel = create_image(image_info["kernel"])
+            print(f"Creating kernel '{disk['kernel']}'")
+            clone(kernel, disk["kernel"])
+    if "initrd" in image_info:
+        disk["initrd"] = store.vm_path(vm_name, "initrd")
+        if not os.path.isfile(disk["initrd"]):
+            initrd = create_image(image_info["initrd"])
+            print(f"Creating initrd '{disk['initrd']}'")
+            clone(initrd, disk["initrd"])
+    if "kernel_parameters" in image_info:
+        disk["kernel_parameters"] = image_info["kernel_parameters"]
     return disk
 
 
