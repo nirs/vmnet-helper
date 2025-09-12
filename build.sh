f47a0e1f791732cded6c1527fb0100a26b58dd3c
Nir Soffer <nirsof@gmail.com>
2025-09-12 17:13:43 +0300

    build: Build universal binary
    meson does not support well building universal binaries so we use a
    build script to build arm64 and x86_64 binaries, create universal
    binaries, install them to a temporary directory, and create an archive
    with the built executables.
    
    Local development can be done using meson commands, but in the CI we use
    the build script.
    
    Building a release tarball with universal binaries:
    
        ./build.sh build
    
    Build directory structure:
    
        % tree -L1 build
        build
        ├── arm64
        ├── root
        ├── vmnet-client
        ├── vmnet-helper
        ├── vmnet-helper.tar.gz
        └── x86_64

---
+++ build.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: The vmnet-helper authors
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+build_dir="${1:?Usage: $0 BUILD_DIR}"
+
+for arch in arm64 x86_64; do
+    meson setup "$build_dir/$arch" --cross-file "$arch.ini"
+    meson compile -C "$build_dir/$arch"
+done
+
+for prog in vmnet-helper vmnet-client; do
+    lipo -create "$build_dir/x86_64/$prog" "$build_dir/arm64/$prog" -output "$build_dir/$prog"
+done
+
+root_dir="$build_dir/root"
+prefix="/opt/vmnet-helper"
+
+rm -rf "$root_dir"
+
+bin_dir="$root_dir$prefix/bin"
+
+install -v -d -m 0755 "$bin_dir"
+for prog in vmnet-helper vmnet-client; do
+    install -v -m 0755 $build_dir/$prog $bin_dir
+done
+
+sudoers_dir="$root_dir$prefix/share/doc/vmnet-helper/sudoers.d"
+
+install -v -d -m 0755 "$sudoers_dir"
+install -v -m 0644 sudoers.d/README.md "$sudoers_dir"
+install -v -m 0644 sudoers.d/vmnet-helper "$sudoers_dir"
+
+archive="$PWD/$build_dir/vmnet-helper.tar"
+
+# Make content reproducible by using commit time instead of build time.
+commit_time=$(git log -1 --pretty=%ct)
+commit_time_iso8601=$(date -u -Iseconds -r "$commit_time" | sed -e 's/+00:00/Z/')
+find "$root_dir" -exec touch -d "$commit_time_iso8601" {} \;
+
+rm -f "$archive"
+
+# Create reproducible archive by sorting the files and overriding uid/gid.
+(cd "$root_dir" && find -s .) | \
+    tar --create \
+        --file $archive \
+        --uid 0 \
+        --gid 0 \
+        --verbose \
+        --no-recursion \
+        --directory "$root_dir" \
+        --files-from /dev/stdin
+
+# Create reproducible file by disabling filename and timestamp.
+gzip -9 --no-name $archive
