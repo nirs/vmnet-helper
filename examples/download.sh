8ce9902c0cff80786138ba898619a1daa820de1e
Nir Soffer <nirsof@gmail.com>
2025-10-11 15:43:50 +0300

    Update examples to Ubuntu 25.04
    Matching the same image used in the python example.

--- examples/download.sh
+++ examples/download.sh
@@ -16,7 +16,7 @@ curl \
     --fail \
     --location \
     --output "$TMP_IMAGE" \
-    "https://cloud-images.ubuntu.com/releases/24.10/release/ubuntu-24.10-server-cloudimg-$IMAGE_ARCH.img"
+    "https://cloud-images.ubuntu.com/releases/25.04/release/ubuntu-25.04-server-cloudimg-$IMAGE_ARCH.img"
 
 # Convert to raw so we can use this image for vfkit.
 qemu-img convert -f qcow2 -O raw "$TMP_IMAGE" "$DISK_IMAGE"
